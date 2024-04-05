9e843674237b1464fe7fc029f726e13c9ae24fe7
Mehdi <mehdi.mahtallah31@gmail.com>
2024-04-05 10:46:13 +0200

    AjoutFixtures

--- src/DataFixtures/LieuFixture.php
+++ src/DataFixtures/LieuFixture.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Lieu;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use phpDocumentor\Reflection\Types\Self_;
 
